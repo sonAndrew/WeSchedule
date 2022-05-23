30986545f999cd9c887ef92f39bea71f06459c43
sonandrew <andrewz.jackson667@gmail.com>
2022-05-23 11:22:35 -0500

    Created: created Sections folder.

--- src/Pages/WeSchedule/Overview/Overview.js
+++ src/Pages/WeSchedule/Overview/Overview.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 export const Overview = () => {
     return (
-        <div>Overview
-             <div>Overview</div>
-             <div>Overview</div>
-             <div>Overview</div>
-        </div>
+        <div>Overview</div>
     )
 }
