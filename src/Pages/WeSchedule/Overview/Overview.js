804f95997dbf05c73ac16a96a4ea5e9f19b3e8ed
williamacbrown <williamacbrown@gmail.com>
2022-05-23 16:19:45 -0400

    Updated: Added a graph with two dummy employees

--- src/Pages/WeSchedule/Overview/Overview.js
+++ src/Pages/WeSchedule/Overview/Overview.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 // Todo 
 // import chart component
+import LineChart from "../Sections/Charts/Chart";
 // import schedule component
 // import location component
 // import department component
 
 export const Overview = () => {
     return (
-        <div>Overview</div>
+        <div>Overview
+            <LineChart/>
+        </div>
     )
 }
