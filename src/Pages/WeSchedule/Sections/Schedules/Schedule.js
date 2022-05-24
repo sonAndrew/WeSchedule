3f80a9100695ddba0d93e7cc0ff8d89da34d065e
Tchardley <tk.romulus@gmail.com>
2022-05-24 14:24:40 -0400

    Created: created schedule table with dummy data.

--- src/Pages/WeSchedule/Sections/Schedules/Schedule.js
+++ src/Pages/WeSchedule/Sections/Schedules/Schedule.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+
 // Todo
 // Import the schedule table 
-
+import { ScheduleTable } from './ScheduleTable';
 export const Schedule = () => {
     return (
         <div>
-            
+           <ScheduleTable /> 
+
         </div>
     )
 }
