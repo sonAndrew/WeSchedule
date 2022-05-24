a8a22d80bf455c639dfd775e63650d6f93555bf1
jrnuttallscode <joshuarnuttall@gmail.com>
2022-05-24 13:13:14 -0600

    Created: Added clickability on Overview to show Depatrments info

--- src/Pages/WeSchedule/Overview/Overview.js
+++ src/Pages/WeSchedule/Overview/Overview.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-// Todo 
+// Todo
 // import chart component
 // import schedule component
 // import location component
 // import department component
+import {Departments} from '../Sections/Departments.js/Department';
 
 export const Overview = () => {
     return (
-        <div>Overview</div>
+        <div>Overview
+            <Departments />
+        </div>
     )
 }
